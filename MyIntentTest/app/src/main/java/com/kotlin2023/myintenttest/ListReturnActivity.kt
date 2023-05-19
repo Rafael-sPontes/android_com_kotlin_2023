e5fccf282753ddc567df46520e663bacc2008568
Rafael Pontes <rafael.spontes@hotmail.com>
2023-05-18 23:29:04 -0300

    android_com_kotlin_2023
    Curso - Android Studio Kotlin 2023

--- MyIntentTest/app/src/main/java/com/kotlin2023/myintenttest/ListReturnActivity.kt
+++ MyIntentTest/app/src/main/java/com/kotlin2023/myintenttest/ListReturnActivity.kt
@@ -4,11 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.kotlin2023.myintenttest.databinding.ActivityListReturnBinding
 
 class ListReturnActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListReturnBinding
+    private lateinit var result: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,26 @@ class ListReturnActivity : AppCompatActivity() {
             i.putExtra("txt_item6", "")
 
             startActivity(i)
+        }
+
+        result = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.data != null && it.resultCode == 1) {
+                val txt_item1 = it.data?.getStringExtra("txt_item1").toString()
+                val txt_item2 = it.data?.getStringExtra("txt_item2").toString()
+                val txt_item3 = it.data?.getStringExtra("txt_item3").toString()
+                val txt_item4 = it.data?.getStringExtra("txt_item4").toString()
+                val txt_item5 = it.data?.getStringExtra("txt_item5").toString()
+                val txt_item6 = it.data?.getStringExtra("txt_item6").toString()
+
+                binding.txtItem1.text = txt_item1
+                binding.txtItem2.text = txt_item2
+                binding.txtItem3.text = txt_item3
+                binding.txtItem4.text = txt_item4
+                binding.txtItem5.text = txt_item5
+                binding.txtItem6.text = txt_item6
+            } else {
+                Toast.makeText(applicationContext, "Erro ao atualizar!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
