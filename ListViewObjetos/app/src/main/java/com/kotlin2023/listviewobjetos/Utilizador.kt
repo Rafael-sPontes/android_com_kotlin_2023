8da43b1a049e132c412f259e6a03440963a4f3b4
Rafael Pontes <rafael.spontes@hotmail.com>
2023-05-30 17:47:05 -0300

    android_com_kotlin_2023
    Curso - Android Studio Kotlin 2023

--- ListViewObjetos/app/src/main/java/com/kotlin2023/listviewobjetos/Utilizador.kt
+++
@@ -1,8 +0,0 @@
-package com.kotlin2023.listviewobjetos
-
-class Utilizador (val username: String, val password: String) {
-    override fun toString(): String {
-        //return "Utilizador(username='$username', password='$password')"
-        return "Username: $username"
-    }
-}
