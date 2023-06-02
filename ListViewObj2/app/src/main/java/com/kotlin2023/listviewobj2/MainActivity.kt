ba6e8e54f432ddfe21fc88c34c7f27ea1e85fd98
Rafael Pontes <rafael.spontes@hotmail.com>
2023-06-01 21:30:16 -0300

    android_com_kotlin_2023
    Curso - Android Studio Kotlin 2023

--- ListViewObj2/app/src/main/java/com/kotlin2023/listviewobj2/MainActivity.kt
+++ ListViewObj2/app/src/main/java/com/kotlin2023/listviewobj2/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.kotlin2023.listviewobj2
 
-import android.icu.text.UnicodeSetSpanner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -10,22 +9,65 @@ import com.kotlin2023.listviewobj2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var pos = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*val listUser = ArrayList<Users>()
+        val listUser = ArrayList<Users>()
+
         listUser.add(Users("rafael", "night"))
         listUser.add(Users("lorrane", "nenha"))
         listUser.add(Users("elessandra", "getcha"))
-        listUser.add(Users("sidnei", "elessar"))*/
+        listUser.add(Users("sidnei", "elessar"))
 
-        binding.btnInserir.setOnClickListener {}
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listUser)
+        binding.listViewUsers.adapter = adapter
 
-        binding.btnEditar.setOnClickListener {}
+        binding.listViewUsers.setOnItemClickListener { _, _, position, _ ->
+            val username = listUser.get(position).username
+            val password = listUser.get(position).password
+            binding.editUsername.setText(username)
+            binding.editPassword.setText(password)
+            pos = position
+        }
 
-        binding.btnEliminar.setOnClickListener {}
+        binding.btnInserir.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editPassword.text.toString().trim()
+            listUser.add(Users(username, password))
+            adapter.notifyDataSetChanged()
+            pos = -1
+            Toast.makeText(applicationContext, "Usuário Cadastrado!", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnEditar.setOnClickListener {
+            if (pos >= 0) {
+                val username = binding.editUsername.text.toString().trim()
+                val password = binding.editPassword.text.toString().trim()
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    listUser.get(pos).username = binding.editUsername.text.toString().trim()
+                    listUser.get(pos).password = binding.editPassword.text.toString().trim()
+                    adapter.notifyDataSetChanged()
+                    binding.editUsername.setText("")
+                    binding.editPassword.setText("")
+                    pos = -1
+                    Toast.makeText(applicationContext, "Usuário Atualizado!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.btnEliminar.setOnClickListener {
+            if (pos >= 0) {
+                listUser.removeAt(pos)
+                adapter.notifyDataSetChanged()
+                pos = -1
+                Toast.makeText(applicationContext, "Usuário Removido!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Selecione um Usuário!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
